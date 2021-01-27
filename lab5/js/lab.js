0bc5078ce831106cc2f21ed3a3f5af3d447bb9b0
zeyu zhang <zhangzeyu99@qq.com>
2021-01-27 11:31:54 -0800

    lab5
    a

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+ * Authors: Johnny Choi <cchoi14@ucsc.edu>
+ *      and Zeyu Zhang  <zzhan255@ucsc.edu>
+ * Created: 28 Jan. 2021
+ * License: Public Domain
+ */
+
+// declare variables
+var make = "Hyundai";
+var model = "Elantra";
+var color = "Red";
+var year = 1994;
+
+// calculate age
+var age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
